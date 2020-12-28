35a4fef590041681dbb6cf14e06d70d3c56934ff
ADSWT518 <tangyazhou518@outlook.com>
2020-12-29 00:33:59 +0800

    呜呜呜还没做完

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -43,7 +43,7 @@ windList = [[] for i in range(7)]
 pm25List = [[] for i in range(7)]
 pm25DayList = []
 
-""" 
+
 def gradeOfIAQI(iaqi):
     if iaqi <= 50:
         return 1
@@ -57,7 +57,7 @@ def gradeOfIAQI(iaqi):
         return 5
     else:
         return 6
- """
+
 
 def date_transder(my_str):
     pattern = re.compile('(\d+)\D(\d+)\D(\d+)')
@@ -286,7 +286,6 @@ def display2():
 
 def display3():
     print(pd.value_counts([i for item in windList for i in item]))
-    fig1 = plt.figure()
 
     pm25ForWind = [[] for i in range(6)]
 
@@ -294,54 +293,71 @@ def display3():
     W = [i for item in windList for i in item]
 
     for index in range(len(W)):
-        if PM[int(index / 2)] <= 150:
-            continue
         if W[index] == 1:
-            pm25ForWind[0].append(PM[int(index / 2)])
+            pm25ForWind[0].append(gradeOfIAQI(PM[int(index / 2)]))
         elif W[index] == 2:
-            pm25ForWind[1].append(PM[int(index / 2)])
+            pm25ForWind[1].append(gradeOfIAQI(PM[int(index / 2)]))
         elif W[index] == 3:
-            pm25ForWind[2].append(PM[int(index / 2)])
+            pm25ForWind[2].append(gradeOfIAQI(PM[int(index / 2)]))
         elif W[index] == 4:
-            pm25ForWind[3].append(PM[int(index / 2)])
+            pm25ForWind[3].append(gradeOfIAQI(PM[int(index / 2)]))
         elif W[index] == 5:
-            pm25ForWind[4].append(PM[int(index / 2)])
+            pm25ForWind[4].append(gradeOfIAQI(PM[int(index / 2)]))
         elif W[index] == 6:
-            pm25ForWind[5].append(PM[int(index / 2)])
+            pm25ForWind[5].append(gradeOfIAQI(PM[int(index / 2)]))
         else:
             print("WTF?")
 
     print(len(pm25ForWind))
     print(len(pm25ForWind[0]))
 
-    for i in range(2):
-        for j in range(3):
-            time = np.arange(len(tempList[i * 3 + j]))
+    plt.title('Scores by group and gender')
 
-            pm25PerWind = pm25ForWind[i * 3 + j]
-            print('\n')
-            ax = fig1.add_subplot(2, 3, i * 3 + j + 1)
-            # the histogram of the data
-            ax.hist(pm25PerWind,
-                    bins=30,
-                    facecolor="red",
-                    edgecolor="black",
-                    alpha=0.7)
-            print(np.mean(pm25PerWind))
-            print(np.var(pm25PerWind))
-            print(len(pm25PerWind))
-            ax.axvline(np.mean(pm25PerWind),
-                       color='b',
-                       linestyle='dashed',
-                       linewidth=1)
+    N = 6
+    ind = np.arange(N)  #[ 0  1  2  3  4  5  6  7  8  9 10 11 12]
+    plt.xticks(ind, ('G1', 'G2', 'G3', 'G4', 'G5', 'G6'))
 
-            ax.set_title("PM2.5(>150) when the wind is grade " +
-                         str(i * 3 + j + 1))
-            ax.set_xlabel(r"PM2.5 (IAQI)")
-            ax.set_xlim(140, 400)
+    plt.ylabel('Scores')
+    plt.yticks(np.arange(0, 81, 20))
+
+    # 表示所有1级空气中风速的占比
+    g1 = (pm25ForWind[0].count(1), pm25ForWind[0].count(2),
+          pm25ForWind[0].count(3), pm25ForWind[0].count(4),
+          pm25ForWind[0].count(5), pm25ForWind[0].count(6))
+    g2 = (pm25ForWind[1].count(1), pm25ForWind[1].count(2),
+          pm25ForWind[1].count(3), pm25ForWind[1].count(4),
+          pm25ForWind[1].count(5), pm25ForWind[1].count(6))
+    g3 = (pm25ForWind[2].count(1), pm25ForWind[2].count(2),
+          pm25ForWind[2].count(3), pm25ForWind[2].count(4),
+          pm25ForWind[2].count(5), pm25ForWind[2].count(6))
+    g4 = (pm25ForWind[3].count(1), pm25ForWind[3].count(2),
+          pm25ForWind[3].count(3), pm25ForWind[3].count(4),
+          pm25ForWind[3].count(5), pm25ForWind[3].count(6))
+    g5 = (pm25ForWind[4].count(1), pm25ForWind[4].count(2),
+          pm25ForWind[4].count(3), pm25ForWind[4].count(4),
+          pm25ForWind[4].count(5), pm25ForWind[4].count(6))
+    g6 = (pm25ForWind[5].count(1), pm25ForWind[5].count(2),
+          pm25ForWind[5].count(3), pm25ForWind[5].count(4),
+          pm25ForWind[5].count(5), pm25ForWind[5].count(6))
+    print(g1)
+    d = []
+    for i in range(0, len(g1)):
+        sum = g1[i] + g2[i] + g3[i] + g4[i] + g5[i] + g6[i]
+        d.append(sum)
+
+    width = 0.35  # 设置条形图一个长条的宽度
+    p1 = plt.bar(ind, g1, width)
+    p2 = plt.bar(ind, g2, width, bottom=g1)
+    p3 = plt.bar(ind, g3, width, bottom=g1 + g2)
+    p4 = plt.bar(ind, g4, width, bottom=g1 + g2 + g3)
+    p5 = plt.bar(ind, g5, width, bottom=g1 + g2 + g3 + g4)
+    p6 = plt.bar(ind, g6, width, bottom=g1 + g2 + g3 + g4 + g5)
+
+    plt.legend((p1[0], p2[0], p3[0], p4[0], p5[0], p6[0]),
+               ('g1', 'g2', 'g3', 'g4', 'g5', 'g6'),
+               loc=3)
 
     plt.show()
-
 
 
 if __name__ == '__main__':
